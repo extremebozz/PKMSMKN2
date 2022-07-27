5b839afceab8c365808391409c26cdf2c936f2cf
extreme bozz <roytpi4@gmail.com>
2022-07-27 17:52:38 +0700

    Start report hotel

---
+++ PKMSMKN2/Model/MReportHotel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKMSMKN2.Model
+{
+    internal class MReportHotel
+    {
+        public DateTime TanggalMasuk { get; set; }
+        public DateTime TanggalKeluar { get; set; }
+        public string NomorKamar { get; set; }
+        public int ExtraBed { get; set; }
+        public int Hari { get; set; }
+        public int Rate { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut{ get; set; }
+        public string NamaPelanggan { get; set; }
+    }
+}
